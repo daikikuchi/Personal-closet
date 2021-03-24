c4092736d9a0a6ebf272bbbbe797e8bae50c57b0
dai <kikuchi.dai@gmail.com>
2021-03-24 11:55:00 +0900

    Added Google login

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'allauth.socialaccount.providers.twitter',
+    'allauth.socialaccount.providers.google',
 
     # Local
     'accounts',
