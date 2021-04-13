c95f650dfa315e0211185ac81b062ab741cd61d2
dai <kikuchi.dai@gmail.com>
2021-04-14 08:48:17 +0900

    Created list view for category and its clothes

--- clothes/urls.py
+++ clothes/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
-from .views import BrandListView, BrandClothesListView
+from .views import (BrandListView, BrandClothesListView, CategoryListView,
+                    CategoryClothesListView)
 
 app_name = 'clothes'
 
 urlpatterns = [
-    path('brands/', BrandListView.as_view(), name='brand_list'),
-    path('brands/<int:id>/<slug:slug>/',
+    # Brands URLS
+    path('brand/', BrandListView.as_view(), name='brand_list'),
+    path('brand/<int:id>/<slug:slug>/',
          BrandClothesListView.as_view(),
          name='brand_clothes'),
+
+    # Category URLS
+    path('category/', CategoryListView.as_view(), name='category_list'),
+    path('category/<int:id>/<slug:slug>/',
+         CategoryClothesListView.as_view(),
+         name='category_clothes'),
 ]
